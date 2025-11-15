a66cbe558df690696bb697984f1b8b80ebb05c6c
Anita <anitasierra901@gmail.com>
2025-11-15 17:18:24 -0600

    adding the flavor count

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const amtOfFlav = userInput.split(`,`);
 console.log(amtOfFlav);
 //made amtOfFlav an array
 
+//Can you parse the user input into an array of froyo flavors?
+const stringified = JSON.stringify(amtOfFlav);
+//console.log(stringified);
+//^^made a string out of array, not what I want
+
+//console.log(JSON.parse(amtOfFlav));
 //Can you parse the user input into an array of froyo flavors? yes
 const flavorCount = {};
 amtOfFlav.forEach((flavor) => {
@@ -15,6 +21,6 @@ amtOfFlav.forEach((flavor) => {
     flavorCount[flavor] += 1;
   } else {
     flavorCount[flavor] = 1;
-  }
+  } //or we ^^ set it equal to one
 });
 console.log(flavorCount);
